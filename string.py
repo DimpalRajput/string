b0bc006f75943a2fe40f3ec2b5931adaf1e3cc78
DimpalRajput <cscdimpal7474@gmail.com>
2026-06-23 20:19:46 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,28 @@
+name="jaanvi"#global scope
+def function():#local scope
+    global age
+    age=20
+    print(age)
+function()
+print("hello",name)
+ #data type : 1 numerical data type:integer,float,complex
+x=20
+y=10.5
+print(type(x))
+print(type(y))
+z=complex(x)
+print(z)
+r=int(y)
+print(r)
+h=5+3j
+print(type(h))
+message=" Hello jaanvi this is python program "
+print(len(message))
+print(message[:-1])
+print(message.lower())
+print(message.replace("jaanvi","mahak"))
+if("mahak" in message):
+ print("mahak is present in message")
+else:
+   print("not present in message")
+   print(message.strip())
