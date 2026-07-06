e3739d6d523391855a439230c33b8384f79744e7
DimpalRajput <cscdimpal7474@gmail.com>
2026-07-07 00:21:30 +0530

    testing

--- list.py
+++ list.py
@@ -1,14 +1,14 @@
 # list
 # #creating list
-# # students=["anjali","minakshi","garima","sushant","deepak"]
-# # print(students[3])
-# # print(students)
+# students=["anjali","minakshi","garima","sushant","deepak"]
+# print(students[3])
+# print(students)
 # # #updating list
-# # students[2]="sonali"
-# # print(students)
+# students[2]="sonali"
+# print(students)
 # # #Tuple
-# # #creating tuple
-t1=(23,43,"sonakshi",57.8,43,43)
+#  #creating tuple
+# t1=(23,43,"sonakshi",57.8,43,43)
 # # print(t1)
 # # #slicing
 # print(t1[1:3])
@@ -28,7 +28,7 @@ t1=(23,43,"sonakshi",57.8,43,43)
 # print(my_list)
 # t1=tuple(my_list)
 # print(t1)
-# list3=[34,78,(23,78,67),98,100]
+#list3=[34,78,(23,78,67),98,100]
 # print(len(list3))
 # print(list3.append(65))
 # value=(34,56.7,"priyal",67.8)
@@ -45,32 +45,87 @@ t1=(23,43,"sonakshi",57.8,43,43)
 
 # t3=value+t1
 # print(t3)
-#print(value.index("priyal"))
-first=t1.index(43)
-print(t1[first+1:].index(43)+first+1)
+# print(value.index("priyal"))
+#print(list3[::-1])
 
-
+#allow only unique elememts
+#name={"shruti","priyanka","neer","bhanu","shruti",9,67.8,0,False,"sudhanshu",56}
+#print(name)
+#finding length
+#print(len(name))
+#data type
+#print(type(name))
+#tuple1=("riddhi","priyal",56,78.9,True,"sudhanshu")
+#t1=set(tuple1)
+#print(t1)
+#set2= {}
+#print(type(set2))
+#print(set2)
+#print("riddhi" in name)
+#print("shruti" not in name)
+#for priyanka in name:
+    #print("priyanka")
+#name.add("siddhart")
+#print(name)
+#name.update(t1)
+#print(name)
+#name.remove("neer")
+#print(name)
+#name.discard("priyanka")
+#print(name)
+#union,update,intersection and other methods
+#set3=name.union(t1)
+#print(set3)
+#name.update(t1)
+#print(name)
+#set4=name.intersection(t1)
+#print(set4)
+#name.update(list3)
+#print(name)
+#l1=["dimpalrajput7474@gmail.com","rinkumittal98@gmail.com","dimpalrajput7474@gmail.com","rinkumittal98@gmail.com"]
+#s1=set(l1)
+#print(s1)
 marketing={
-    "product":"mobile phone",
+   "ITEM1":{"product":"mobile phone",
     "sale":156,
-    "profit":"12 lakh"
+    "profit":"12 lakh"},
+    
+    "ITEM2":{"product1":"laptop",
+        "sale1":325,
+        "profit1":"25 lakh"},
 
+    
+    
+    "ITEM3":{"product2":"tablet",
+        "sale2":234,
+        "profit2":"13 lakh"},
+    
 }
-print(marketing)
-print(len(marketing))
-production=dict(item="laptop",total=34)
+marketing.get("product4")
+print("product is not present")
+
+#print(marketing)
+#print(len(marketing))
+#production=dict(item="laptop",total=34)
 #print(type(production))
-print(production)
-l2=[("name","sonakshi"),("height",5.2),("age",13),("t1",(23,45,67)),("value",True)]
-dict1=dict(l2)
-print(dict1)
-print(type(dict1))
+#print(production)
+#l2=[("name","sonakshi"),("height",5.2),("age",13),("t1",(23,45,67)),("value",True)]
+##dict1=dict(l2)
+#print(dict1)
+#print(type(dict1))
 #to check existance of key through sqaure brackets and get method and print a message in get method
-marketing.update({"discount":"5%",
-                 "branch":"new delhi"})
-print(marketing)
-print(marketing.popitem())
-print(marketing)
+#marketing.update({"discount":"5%",
+                 #"branch":"new delhi"})
+#print(marketing)
+#print(marketing.popitem())
+#print(marketing)
+#clear,delete
+python={"naveen","garima","mahima","preeti","janvi"}
+java={"janvi","sumit","naveen","punit"}
+set1=python.intersection(java)
+#print(set1)
+#print(marketing.copy())
+
 
 
 
--- string.py
+++ string.py
@@ -1,28 +1,74 @@
-name="jaanvi"#global scope
-def function():#local scope
-    global age
-    age=20
-    print(age)
-function()
-print("hello",name)
- #data type : 1 numerical data type:integer,float,complex
-x=20
-y=10.5
-print(type(x))
-print(type(y))
-z=complex(x)
-print(z)
-r=int(y)
-print(r)
-h=5+3j
-print(type(h))
-message=" Hello khushbu this is python program "
-print(len(message))
-print(message[:-1])
-print(message.lower())
-print(message.replace("jaanvi","mahak"))
-if("mahak" in message):
- print("mahak is present in message")
+#1.
+name="dimpal"
+age=21
+height=5.7
+print(name,age,height)
+#2.
+a=49
+b=7
+sum=a+b
+diff=a-b
+product=a*b
+division=a/b
+print(sum,diff,product,division)
+#3.
+c=10
+d=10.5
+e="python"
+f=True
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+#4.
+a,b=b,a
+print("a =",a,"b =",b)
+#5.
+c=b=a
+print(a,b,c)
+#6.
+subject="mathematics"
+print(subject[-1])
+print(subject[0])
+#7.
+print(subject[::-1])
+#8.
+print(subject.count("m"))
+#9.
+print(subject.upper())
+print(subject.lower())
+print(subject.title())
+#10.
+if a<0:
+    print("a is negative integer")
+if a>0:
+    print("a is positive integer")
 else:
-   print("not present in message")
-   print(message.strip())
+    print("a is equal to 0")
+#11.
+if a%2==0:
+    print("a is even")
+else:
+    print("a is odd")
+#12.
+if age<18:
+    print("not eligible for voting")
+else:
+    print("eligible for voting")
+#13.
+letter="y"
+if letter in "aeiou":
+    print("letter is vowel")
+else:
+    print("letter is consonant")
+#14.
+list=[23,45,56,78,79]
+for num in list:
+    if num % 2==0:
+        print(num)
+#15.
+
+
+
+
+
