f86dd8eec20d347d77dd65d2015a70e8047bad53
DimpalRajput <cscdimpal7474@gmail.com>
2026-07-08 00:16:57 +0530

    opeators
    opeators are used in this program

---
+++ operator.py
@@ -0,0 +1,39 @@
+#arithematic operator
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(b/a)
+print(b%a)
+print(a**b)
+print(b//a)
+#assignment operator
+print(a)
+a+=5
+print(a)
+a-=5
+print(a)
+a*=5
+print(a)
+a/=5
+print(a)
+a%=5
+print(a)
+a**=5
+print(a)
+a//=5
+print(a)
+#relational operator
+c=23
+d=34
+print(c==d)
+print(c!=d)
+print(c>d)
+print(c<d)
+print(c<=d)
+#logical operator
+print(c<b and c>b)
+print(c<b or c>b)
+print(not c<b)
+
