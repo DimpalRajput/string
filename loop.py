305ed901e3b9c910ff0a7a7dfa8bab475af73555
DimpalRajput <cscdimpal7474@gmail.com>
2026-07-10 10:23:02 +0530

    practice of loop

---
+++ loop.py
@@ -0,0 +1,41 @@
+# # while loop
+# number=1
+# while number<=7:
+#     print("seven wonders",number)
+#     number+=1
+#  #print number from 1 to 100
+# numbers=1
+# while numbers<=100:
+#     print(numbers)
+#     numbers+=1
+# # print number from 100 to 1
+# Nnumber=100
+# while Nnumber>=1:
+#     print(Nnumber)
+#     Nnumber-=1
+# # multiplication table
+# n=int(input("enter number: "))
+# i=1
+# while i<=10:
+#     print(n*i)
+#     i+=1111
+# digit=int(input("enter number:"))
+# i=1
+# while i<=10:
+#     print(i,"->",i**2) 
+#     i+=1
+t1=(1,4,9,16,25,36,49,64,81,100)
+i=0
+while i<len(t1):
+    print(t1[i])
+    if t1[i]==36:
+     print("36 found")
+    break
+i+=1
+
+
+
+
+
+
+
