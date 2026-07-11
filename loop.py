51fb46ae8aec006a7f9dd94796133595ef9009d7
DimpalRajput <cscdimpal7474@gmail.com>
2026-07-11 22:02:18 +0530

    loop changes

---
+++ loop.py
@@ -0,0 +1,42 @@
+# while loop
+number=1
+while number<=7:
+    print("seven wonders",number)
+    number+=1
+ #print number from 1 to 100
+numbers=1
+while numbers<=100:
+    print(numbers)
+    numbers+=1
+# print number from 100 to 1
+Nnumber=100
+while Nnumber>=1:
+    print(Nnumber)
+    Nnumber-=1
+# multiplication table
+n=int(input("enter number: "))
+i=1
+while i<=10:
+    print(n*i)
+    i+=1111
+digit=int(input("enter number:"))
+i=1
+while i<=10:
+    print(i,"->",i**2) 
+    i+=1
+t1=(1,4,9,16,25,36,49,64,81,100)
+i=0
+while i<len(t1):
+    print(t1[i])
+i+=1
+#for loop
+l1=["simran","tarun","vinay","vaibhav"]
+for "tarun" in l1:
+    print("name found")
+
+
+
+
+
+
+
